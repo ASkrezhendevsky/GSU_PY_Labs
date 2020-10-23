928d560148acd985fc8a3007af7df488d8fc6a7c
ASkrezhendevsky <43349565+ASkrezhendevsky@users.noreply.github.com>
2020-10-23 14:49:39 +0300

    Pr_1

---
+++ GSU_PY_PR_1/main.py
@@ -0,0 +1,46 @@
+def encryptSimbol(alphabet, symbol, key):
+    if symbol.upper() not in alphabet:
+        return symbol
+    if symbol.islower():
+        return alphabet[(alphabet.index(symbol.upper()) + key) % len(alphabet)].lower()
+    else:
+        return alphabet[(alphabet.index(symbol.upper()) + key) % len(alphabet)]
+
+
+def encryptString(string, key, alphabets):
+    Output = []
+    for symbol in string:
+        currentAlphabet = alphabets[0]
+        for alph in alphabets:
+            if symbol.upper() in alph:
+                currentAlphabet = alph
+                break
+        Output.append(encryptSimbol(currentAlphabet, symbol, key))
+
+    return "".join(Output)
+
+
+alphabets = [
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+     'X', 'Y', 'Z'],
+    ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц',
+     'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']]
+print("Enter string")
+InputString = input()
+
+print("Enter mode number\n"
+      "1 - decrypt\n"
+      "2 - encrypt\n"
+      "3 - breaking\n")
+
+mode = int(input())
+
+if mode == 1 or mode == 2:
+    print("Enter key")
+    key = int(input())
+    if mode == 1:
+        key = -key
+    print(encryptString(InputString, key, alphabets))
+elif mode == 3:
+    for i in range(0, 40):
+        print(encryptString(InputString, i, alphabets))
