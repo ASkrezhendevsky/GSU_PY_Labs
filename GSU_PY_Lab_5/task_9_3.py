aad479fdf2dc085f9fdf327ecb29fa929591f412
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2020-12-08 00:06:04 +0300

    lab_6

--- GSU_PY_Lab_5/task_9_3.py
+++ GSU_PY_Lab_5/task_9_3.py
@@ -23,4 +23,12 @@
 диапазоны адресов и так далее, так как обрабатывается вывод команды, а не ввод пользователя.
 
 '''
+import re
 
+
+def parch_cfg(file_name):
+    inputFile = open(file_name, mode="r")
+    fileText = inputFile.read()
+    if re.search(reg, line):
+            print(line)
+    re.findall()
