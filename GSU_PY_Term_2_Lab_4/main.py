da6fc936061e6dbf3a0e0529e923193cc41a8f8c
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2021-04-09 23:05:14 +0300

    lab 4 index year

--- GSU_PY_Term_2_Lab_4/main.py
+++ GSU_PY_Term_2_Lab_4/main.py
@@ -12,21 +12,26 @@ soup = BeautifulSoup(resp.text, 'lxml')
 writer_id_re = re.compile("^writer-tt")
 rating = 'ratingValue'
 
-films = {"name": [], "rating": []}
+films = {"year": [], "name": [], "rating": []}
 
 films_tags = soup.findAll("div", {'id': writer_id_re})
 for film_tag in films_tags:
     film_ref = film_tag.b.a['href']
-    flim_name = film_tag.b.a.string
+    film_name = film_tag.b.a.string
+    film_year = film_tag.span.string
     film_soup = BeautifulSoup(req.get(site + film_ref).text, 'lxml')
     film_rating = film_soup.find("span", {'itemprop': rating})
     print(film_rating)
     if film_rating:
-        print(film_tag['class'], film_ref, flim_name, film_rating.string)
-        films["name"].append(flim_name)
+        print(film_tag['class'], film_ref, film_name, film_rating.string)
+        films["year"].append(film_year)
+        films["name"].append(film_name)
         films["rating"].append(float(film_rating.string))
 
-films_table = pd.DataFrame(films)
+films["year"].reverse()
+films["name"].reverse()
+films["rating"].reverse()
+films_table = pd.DataFrame(films, index=films["year"])
 
 plot = films_table.plot(figsize=(8, 6), linewidth=2, )
 
