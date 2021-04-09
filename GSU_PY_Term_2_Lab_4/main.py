4f531577d06df7cc059109dd52a69a0287918c06
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2021-04-09 17:52:54 +0300

    lab 4 done

--- GSU_PY_Term_2_Lab_4/main.py
+++ GSU_PY_Term_2_Lab_4/main.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
 import requests as req
 import re
 
@@ -7,12 +9,25 @@ site = 'https://www.imdb.com'
 resp = req.get(site + "/name/nm0000233/")
 soup = BeautifulSoup(resp.text, 'lxml')
 
-films_tags = soup.findAll("div", {'id': re.compile("^writer-tt")})
+writer_id_re = re.compile("^writer-tt")
+rating = 'ratingValue'
+
+films = {"name": [], "rating": []}
+
+films_tags = soup.findAll("div", {'id': writer_id_re})
 for film_tag in films_tags:
-    flim_ref = film_tag.b.a['href']
+    film_ref = film_tag.b.a['href']
     flim_name = film_tag.b.a.string
-    film_soup = BeautifulSoup(req.get(site + flim_ref).text, 'lxml')
-    film_rating = film_soup.find("span", {'itemprop': 'ratingValue'})
+    film_soup = BeautifulSoup(req.get(site + film_ref).text, 'lxml')
+    film_rating = film_soup.find("span", {'itemprop': rating})
     print(film_rating)
     if film_rating:
-        print(film_tag['class'], flim_ref, flim_name, film_rating.string)
+        print(film_tag['class'], film_ref, flim_name, film_rating.string)
+        films["name"].append(flim_name)
+        films["rating"].append(float(film_rating.string))
+
+films_table = pd.DataFrame(films)
+
+plot = films_table.plot(figsize=(8, 6), linewidth=2, )
+
+plt.show()
