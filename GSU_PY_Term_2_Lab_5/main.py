64fdbdd925dbd911465d41e81b9b0295888b6382
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2021-04-30 11:42:01 +0300

    lab 5 resize fix

--- GSU_PY_Term_2_Lab_5/main.py
+++ GSU_PY_Term_2_Lab_5/main.py
@@ -6,27 +6,26 @@ import math
 import os
 
 
-def resize_to_MP(picture, pixel_amount):
-    x = picture.size[0]
-    y = picture.size[1]
+def get_new_size(pixel_amount, current_size):
+    x = current_size[0]
+    y = current_size[1]
 
     x_new = math.sqrt(pixel_amount / x * y)
     y_new = x_new / x * y
 
     target_size = (int(x_new), int(y_new))
-    print(target_size)
-    picture.resize(target_size)
+    return target_size
 
 
 output_image_path = "output/"
 input_image_path = "input/"
-author_image_name = "AuthorImage.jpg"
-author_image_size = (100, 100)
+author_image_name = "AuthorImage.png"
+author_image_size = (200, 200)
 target_pixel_amount = 2000000
 author_tag_id = 315
 date_tag_id = 306
 text_color = (50, 50, 50)
-font = ImageFont.truetype("arial.ttf", 250)
+
 
 for directory, folder, files in os.walk(input_image_path):
     print(directory, folder, files)
@@ -34,20 +33,18 @@ for directory, folder, files in os.walk(input_image_path):
 image_names = files
 
 author_photo = Image.open(author_image_name)
-author_photo.resize(author_image_size)
+author_photo = author_photo.resize(author_image_size)
 
 for image in image_names:
     photo = Image.open(input_image_path + image)
 
-    resize_to_MP(photo, target_pixel_amount)
+    photo = photo.resize(get_new_size(target_pixel_amount, photo.size))
+    if photo.size[0] < photo.size[1]:
+        photo = photo.rotate(90, expand=True)
 
     drawing = ImageDraw.Draw(photo)
 
-    if photo.size[0] < photo.size[1]:
-        drawing.rotate()
-
     photo_info = photo.getexif()
-    print(photo_info)
     try:
         text = photo_info[author_tag_id]
     except:
@@ -56,9 +53,11 @@ for image in image_names:
         text += photo_info[date_tag_id]
     except:
         text += "none"
+
+    font = ImageFont.truetype("arial.ttf", int(photo.size[1] * 0.1))
     drawing.text((20, photo.size[1] * 0.9), text, fill=text_color, font=font)
 
-    photo.paste(author_photo, (photo.size[1] - author_image_size[0], 0))
+    photo.paste(author_photo, (photo.size[0] - author_image_size[0], 0))
 
     qr = qrcode.make('test text')
     photo.paste(qr)
